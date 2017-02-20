5ffd3620ae810f2c549c56452f910974dd11c70f
Lluvio <caotousir@gmail.com>
2017-02-20 22:34:55 +0800

    update sync.sh

--- sync.sh
+++ sync.sh
@@ -3,26 +3,29 @@
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
-function macInit() {
+function doDarwinInit() {
   echo -e "${GREEN} mac init successful ${NC}"
   # 鼠标滚轮方向 false 与windows一致 true 则是保持原始
   defaults write -g com.apple.swipescrolldirection -bool false
 }
 
+function doSync() {
+  echo -e "current operator ${GREEN} $1 ${NC}"
+  echo -e "ignore from ${GREEN} $PWD/other/exclude-$1.list ${NC}"
+
+  rsync --exclude-from="$PWD/other/exclude-$1.list" \
+    -avh --no-perms . ~;
+  ln -s $PWD/.bin/$1/* ~/.bin
+}
+
 function doIt() {
+  # darwin or linux
   operator=`echo $(uname) | tr "A-Z" "a-z"`
   if [ $operator = "Linux" ] ; then
-    echo -e "current operator ${GREEN} $operator ${NC}"
-    rsync --exclude-from="$PWD/other/exclude-$operator.list" \
-      -avh --no-perms . ~;
-    ln -s $PWD/.bin/${operator}/* ~/.bin/
+    doSync $operator;
   else
-    # macos
-    echo -e "current operator ${GREEN} $operator ${NC}"
-    rsync --exclude-from="$PWD/other/exclude-$operator.list" \
-      -avh --no-perms . ~;
-    ln -s $PWD/.bin/${operator}/* ~/.bin/
-    macInit;
+    doSync $operator;
+    doDarwinInit;
   fi
 }
 
@@ -38,4 +41,5 @@ else
 fi;
 
 unset doIt;
-unset macInit;
+unset doSync;
+unset doDarwinInit;
