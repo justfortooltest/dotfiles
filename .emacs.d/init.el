421b57448ed4afddaf5191580ec1b45e53a3c73c
Lluvio <caotousir@gmail.com>
2017-02-23 08:06:59 +0800

    bind M-x with helm-M-x

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -31,13 +31,7 @@
 (global-set-key [?\C-x ?h] 'help-command)
                          ;; overrides mark-whole-buffer')')
 
-; (require 'smex) ; Not needed if you use package.el
-; (smex-initialize) ; Can be omitted. This might cause a (minimal) delay
-;                   ; when Smex is auto-initialized on its first run.')
-; (global-set-key (kbd "M-x") 'smex)
-; (global-set-key (kbd "M-X") 'smex-major-mode-commands)
-; ;; This is your old M-x.
-; (global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
+(global-set-key (kbd "M-x") 'helm-M-x)
 
 (setq
  backup-by-copying t    ;自动备份
