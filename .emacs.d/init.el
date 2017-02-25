6c2cf39fc66730080a565246dd735664c4613c71
Lluvio <caotousir@gmail.com>
2017-02-25 10:53:16 +0800

    setq gc-cons-threshold 100mb

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -3,9 +3,11 @@
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
 
+;; gc-cons-threshold 指定了垃圾回收前允许有多少字节的consing
+(setq gc-cons-threshold 100000000)
+
 ;; UTF-8 as default encoding
 (set-language-environment "UTF-8")
-
 (add-to-list 'load-path "~/.emacs.d/lisp/")
 (require 'init-site-lisp)
 (require 'init-elpa)      ;; Machinery for installing required packages
