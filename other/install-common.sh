cb724a21b3afe86b7f9d0b2decc1706bc89df75d
Lluvio <caotousir@gmail.com>
2017-02-28 16:00:15 +0800

    add vim plug install

--- other/install-common.sh
+++ other/install-common.sh
@@ -1,2 +1,6 @@
 # zsh-autosuggestions
 git clone git://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/plugins/zsh-autosuggestions
+
+# install plug.vim
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
