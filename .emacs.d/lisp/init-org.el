364f162b5278c4ff6c0ac3e01287907199d5ebb4
Lluvio <caotousir@gmail.com>
2017-02-28 13:38:42 +0800

    ido vertical & ido fuzzy

--- .emacs.d/lisp/init-org.el
+++ .emacs.d/lisp/init-org.el
@@ -2,11 +2,18 @@
 (global-set-key "\C-cl" 'org-store-link)
 (global-set-key "\C-ca" 'org-agenda)
 (global-set-key "\C-cb" 'org-iswitchb)
+
 (setq org-log-done t ;; CLOSED:[timestamp]
       org-startup-truncated nil ;; 在 org mode 模式下段落随着窗口大小换行
       org-odt-preferred-output-format "doc" ;; 导出 odt 格式为 doc
       org-tags-column 80 ;; 设置tags保持多少column, 同level tag 对齐
+      org-completion-use-ido t
       )
+
+(setq org-goto-interface 'outline-path-completion
+      org-goto-max-level 10)
+
+(setq org-outline-path-complete-in-steps nil)
 
 ;; Note: 如果需要导出 epub/mobi...可以先导出markdown然后通过pandoc转换
 ;; 先转换成 epub, 然后可以通过 calibre 转换你想要的格式
